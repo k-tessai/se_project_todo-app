be9805c0dac5bbf9765d87df31bdff8eaada3b0e
tessburrow <tess_burrow@yahoo.com>
2025-03-25 20:20:54 -0500

    refactoring: Section class

---
+++ components/Section.js
@@ -0,0 +1,18 @@
+class Section {
+  constructor({ items, renderer, containerSelector }) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems() {
+    this._items.forEach((item) => {});
+    //call the renderer, and pass the item as an argument
+  }
+}
+
+addItem(element) {
+// add element to container  
+}
+
+export default Section;
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,25 @@ console.log(uuidv4());
 import { initialTodos } from "../utils/constants.js";
 import { validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
+import Section from "../components/section.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = document.forms["add-todo-form"];
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+const section = new Section({
+  items: [], //pass initial todos
+  renderer: () => {
+    // generate todo item
+    //add it to the todo list -- append
+    // refer to the forEach loop in this file
+  },
+  containerSelector: ".todos__list",
+});
+
+//call section instance's renderItems method
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
@@ -21,41 +34,10 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-// The logic in this function should all be handled in the Todo class.
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
   return todoElement;
-
-  // to be removed
-  // const todoElement = todoTemplate.content
-  //   .querySelector(".todo")
-  //   .cloneNode(true);
-  // const todoNameEl = todoElement.querySelector(".todo__name");
-  // const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-  // const todoLabel = todoElement.querySelector(".todo__label");
-  // const todoDate = todoElement.querySelector(".todo__date");
-  // const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
-
-  // todoNameEl.textContent = data.name;
-  // todoCheckboxEl.checked = data.completed;
-
-  // // Apply id and for attributes.
-  // // The id will initially be undefined for new todos.
-  // todoCheckboxEl.id = `todo-${data.id}`;
-  // todoLabel.setAttribute("for", `todo-${data.id}`);
-
-  // Tess -- to do -- date
-  // // If a due date has been set, parsing this it with `new Date` will return a
-  // // number. If so, we display a string version of the due date in the todo.
-  //
-  // }
-
-  // todoDeleteBtn.addEventListener("click", () => {
-  //   todoElement.remove();
-  // });
-
-  //return todoElement;
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -77,7 +59,6 @@ addTodoForm.addEventListener("submit", (evt) => {
   const dateInput = evt.target.date.value;
   const todoData = { name: name, date: dateInput };
 
-  // Create a date object and adjust for timezone
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
@@ -96,6 +77,10 @@ addTodoForm.addEventListener("submit", (evt) => {
 
 initialTodos.forEach((item) => {
   renderTodo(item);
+
+  // initialTodos.forEach(item) => {
+  // const todo = generateTodo(item);
+  // todosList.append(todo);} -- this is what Kevin's initialTodos looks like -- use add item method instead of append
 });
 
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
